26882f0e9080f7127796e27eaae37b20c2c86ac2
isamardzija <ivansam382@gmail.com>
2022-08-15 10:29:48 +0200

    function that validates whether the input is a valid PIN code format

---
+++ 7kyu/regexValidatePINCode.js
@@ -0,0 +1,19 @@
+// DESCRIPTION:
+
+    // ATM machines allow 4 or 6 digit PIN codes and PIN codes cannot contain anything but exactly 4 digits or exactly 6 digits.
+
+    // If the function is passed a valid PIN string, return true, else return false.
+
+    // Examples (Input --> Output)
+    // "1234"   -->  true
+    // "12345"  -->  false
+
+// URL:
+    // https://www.codewars.com/kata/55f8a9c06c018a0d6e000132/
+
+// SOLUTION:
+
+function validatePIN (pin) {
+    const reg = new RegExp(`^([0-9]{4}|[0-9]{6})$`); 
+    return reg.test(pin)
+  }
