68ab9f133fb098acd732f3abd92ab5d300d90a32
isamardzija <ivansam382@gmail.com>
2022-09-04 18:48:11 +0200

    only return numbers in an array

---
+++ 7kyu/listFiltering.js
@@ -0,0 +1,24 @@
+// DESCRIPTION:
+
+// In this kata you will create a function that takes a list of non-negative integers and strings and returns a new list with the strings filtered out.
+// Example
+
+// filter_list([1,2,'a','b']) == [1,2]
+// filter_list([1,'a','b',0,15]) == [1,0,15]
+// filter_list([1,2,'aasf','1','123',123]) == [1,2,123]
+
+// URL:
+
+// https://www.codewars.com/kata/53dbd5315a3c69eed20002dd
+
+// SOLUTION:
+
+function filter_list(l) {
+  result = [];
+  l.forEach((el) => {
+    if (typeof el == "number") {
+      result.push(el);
+    } else return;
+  });
+  return result;
+}
